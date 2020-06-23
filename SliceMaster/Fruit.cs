8086fdf9920003f9e5e1cee38902225789f24c9d
Tashkovv <jovcotaskov@gmail.com>
2020-06-23 17:47:00 +0200

    Added abstract class Fruit

---
+++ SliceMaster/Fruit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SliceMaster
+{
+    public abstract class Fruit
+    {
+        public string Name { get; set; }
+
+    }
+}
